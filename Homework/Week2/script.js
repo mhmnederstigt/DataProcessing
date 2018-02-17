970c201c89eaf0c919f4f239af350665f08230fb
mhmnederstigt <31936545+mhmnederstigt@users.noreply.github.com>
2018-02-17 22:22:11 +0100

    Proper

--- Homework/Week2/script.js
+++ Homework/Week2/script.js
@@ -63,8 +63,9 @@ window.onload  = function() {
         if (this.readyState == 4 && this.status == 200) {
            // Typical action to be performed when the document is ready:
            document.getElementById("rawdata").innerHTML = xhttp.responseText;
-         
-    
+        }
+    }
+   
     var data = document.getElementById("rawdata").textContent.split("\n")
     arrayX = []
     arrayID = []
