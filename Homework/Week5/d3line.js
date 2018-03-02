6641b00e6e8171ea0586197a29bd55a3b8a4dad7
mhmnederstigt <31936545+mhmnederstigt@users.noreply.github.com>
2018-03-02 16:52:33 +0100

    Update Homework

--- Homework/Week5/d3line.js
+++ Homework/Week5/d3line.js
@@ -1,7 +1,15 @@
 window.onload = function() {
 
-  d3.json("KNMI_20180217.json", function(data) {
+  d3.json("KNMI_20180231.json", function(data) {
+    console.log("Dictionary with lists:");
     console.log(data);
   });
+
+  d3.json("KNMI_20170231.json", function(data) {
+    console.log("List with dictionaries:");
+    console.log(data);
+  });
+
+  console.log("Consequent years in data folder")
 
 };
--- Homework/Week5/helper_scripts/convertCSV2JSON_dl.py
+++ Homework/Week5/helper_scripts/convertCSV2JSON_dl.py
@@ -34,7 +34,7 @@ else:
 		for row in reader:
 			x.append(row[list(row.keys())[0]])
 			y.append(row[list(row.keys())[1]])
-			y.append(row[list(row.keys())[2]])
+			z.append(row[list(row.keys())[2]])
 
 		dl.append({list(row.keys())[0]: x, list(row.keys())[1]: y, list(row.keys())[2]: z})
 					
